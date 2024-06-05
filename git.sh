38a9a9db112b4a71a1ff1c3e6c76cc073834fb51
husain <husain68644@gmail.com>
2024-06-05 12:54:52 +0530

    .

--- git.sh
+++ git.sh
@@ -1,7 +1,5 @@
 git status
 git add .
 git commit -m "."
-$ git remote add origin git@github.com:Husain644/realEstate.git
-
-
-$ git remote -v
+git remote add origin git@github.com:Husain644/realEstate.git
+git remote -v
